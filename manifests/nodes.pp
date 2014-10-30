2537fcadca153f0f5e9b8a4d873bc5a6bc1c3724
junejung <github.com@jueunjung.com>
2014-10-30 14:46:54 -0700

    add group declare dependency with require

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,10 +3,15 @@ node default {
 
 user { 'jjung':
 	ensure => 'present',
-	groups => ['sudo'],
+	groups => ['sudo', 'ubuntu'],
 	home => '/home/jjung',
 	managehome => true,
 	password => '$6$lY2Gp3Cr$zNrUB7T3yibUF/gWn5cTQ0fNv7MUmx/DZuw3E7I..Vh9tITG28BtgvXJPU4Gm4Z/9oNvlbX24KzQ9Ib1QH1B9.',
-	shell => '/bin/bash'
-
+	shell => '/bin/bash',
+	require => User['ubuntu']
 }
+
+user { 'ubuntu':
+	ensure => 'present'
+}
+

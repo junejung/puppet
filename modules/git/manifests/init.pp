b982991736180bc8589588bc815776786ec8afac
junejung <github.com@jueunjung.com>
2014-11-03 13:49:38 -0800

    define custom type for git clone

--- modules/git/manifests/init.pp
+++ modules/git/manifests/init.pp
@@ -7,3 +7,12 @@ class git::install{
 		ensure => present
 	}
 }
+
+#define statement is used to create a custom type that can be used to eleswhere in the manifest, like a function in other languages
+define git::clone ( $path, $dir){
+	exec { 'clone-$name-$path':
+		command => '/usr/bin/git clone git@github.com:$name $path/$dir',
+		creates => '$path/$dir',
+		require => [Class['git'], File[$path]],
+	}
+}
